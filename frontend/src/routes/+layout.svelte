7de0eecea5efd06c88a529fb91fbbe2b8941ecd8
bautista-aa <adrian.bautista@csu.fullerton.edu>
2024-11-14 10:14:58 -0800

    supabase

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script>
-	import '../globals.css';
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
 	import Navbar from '../lib/components/navbar.svelte';
+	import '../globals.css';
+
+	export let data;
+	$: ({ session, supabase } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <Navbar />
