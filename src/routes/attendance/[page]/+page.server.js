84ec3d8f697ea9e8a10827edf2616f8d1b2ff296
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-12-28 12:53:15 -0800

    implement attendance logs page

--- src/routes/attendance/[page]/+page.server.js
+++ src/routes/attendance/[page]/+page.server.js
@@ -13,6 +13,7 @@ export const load = async ({ params, locals: { supabase, safeGetSession } }) => 
 		.from("attendance_logs")
 		.select("*")
 		.eq("user_id", user.id)
+		.order("created_at", { ascending: false })
 		.range(0 + (pageNumber - 1) * 8, 7 + (pageNumber - 1) * 8);
 	if (attendanceLogsQuery.error) {
 		error(500, attendanceLogsQuery.error.message);
@@ -26,8 +27,24 @@ export const load = async ({ params, locals: { supabase, safeGetSession } }) => 
 		error(500, "Server Error");
 	}
 
+	const attendanceLogs = [];
+	const attendanceData = attendanceLogsQuery.data;
+
+	for (const entry of attendanceData) {
+		const sessionKey = entry.session_key;
+		const [department, siCourse, siLeader] = sessionKey.split(";");
+		const { default: departmentSchedule } = await import(`$lib/schedules/${department}.json`);
+
+		for (const session of departmentSchedule.sessionsOffered[siCourse]) {
+			if (session.siLeader === siLeader) {
+				attendanceLogs.push({ loggedAt: entry.created_at, siCourse, siLeader });
+				break;
+			}
+		}
+	}
+
 	return {
-		attendanceLogs: attendanceLogsQuery.data ?? [],
+		attendanceLogs,
 		pageNumber,
 		numLogs: numLogsQuery.count
 	};
--- src/routes/attendance/[page]/+page.svelte
+++ src/routes/attendance/[page]/+page.svelte
@@ -1,7 +1,66 @@
 <script>
+	import AttendanceLogs from "$lib/components/attendance-logs.svelte";
+
 	export let data;
 
 	let attendanceLogs = data.attendanceLogs;
 	let pageNumber = data.pageNumber;
+
 	let numLogs = data.numLogs;
+	let maxPages = Math.floor(numLogs / 8) + 1;
 </script>
+
+<section>
+	<h2>Attendance Logs</h2>
+	<AttendanceLogs {attendanceLogs} />
+	<div class="page-container">
+		{#if pageNumber > 1}
+			<a data-sveltekit-reload href="/attendance/{pageNumber - 1}">{pageNumber - 1}</a>
+		{/if}
+		<span>{pageNumber}</span>
+		{#if pageNumber < maxPages}
+			<a data-sveltekit-reload href="/attendance/{pageNumber + 1}">{pageNumber + 1}</a>
+		{/if}
+	</div>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		max-width: 1280px;
+		margin-inline: auto;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.page-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.75rem;
+	}
+
+	.page-container > a,
+	.page-container > * {
+		text-decoration: none;
+		color: rgb(var(--color-text));
+		padding: 0.75rem 1rem;
+		width: 50px;
+		text-align: center;
+		border-radius: 0.5rem;
+	}
+
+	.page-container > span {
+		background-color: rgb(var(--color-foreground));
+	}
+
+	.page-container > a {
+		background-color: rgb(var(--color-background-500));
+		transition: opacity 150ms ease-in;
+	}
+
+	.page-container > a:hover {
+		opacity: 0.8;
+	}
+</style>
